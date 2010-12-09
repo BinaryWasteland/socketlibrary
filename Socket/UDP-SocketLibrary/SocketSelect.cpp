1af9d47686b88a4f2801332f96c1b62a2634e117
greg.r.jacobs@gmail.com <greg.r.jacobs@gmail.com@6ecfc60f-d6d6-e1e8-1c63-cb8d0dac36c7>
2010-12-09 22:40:13 +0000

    Added UDP project to the Solution

---
+++ Socket/UDP-SocketLibrary/SocketSelect.cpp
@@ -0,0 +1,58 @@
+/** @file: SocketSelect.cpp
+	@author Greg R. Jacobs
+	@author greg.r.jacobs@gmail.com
+	@author http://gregrjacobs.com
+	@date 2010-12-09
+	@version 0.0.0
+	@note Developed for Visual C++ 10.0
+	@brief ...
+	*/
+
+#include "SocketSelect.hpp"
+
+SocketSelect::SocketSelect(Socket const * const s1, Socket const * const s2, TypeSocket type) {
+	FD_ZERO(&fds_);
+	FD_SET(const_cast<Socket*>(s1)->s_,&fds_);
+	if(s2) {
+		FD_SET(const_cast<Socket*>(s2)->s_,&fds_);
+	}     
+
+	TIMEVAL tval;
+	tval.tv_sec  = 0;
+	tval.tv_usec = 1;
+
+	TIMEVAL *ptval;
+	if(type==NonBlockingSocket) {
+		ptval = &tval;
+	}
+	else { 
+		ptval = 0;
+	}
+
+	if (select (0, &fds_, (fd_set*) 0, (fd_set*) 0, ptval) == SOCKET_ERROR) 
+	throw "Error in select";
+}
+
+bool SocketSelect::Readable(Socket const* const s) {
+	if (FD_ISSET(s->s_,&fds_)) return true;
+	return false;
+}
+/*=============================================================
+
+Revision History
+
+Version 0.0.0: 2010-12-09
+Alpha release.
+
+=============================================================
+
+Copyright Greg R. Jacobs
+
+The copyright to the computer program(s) herein
+is the property of Greg R. Jacobs of Canada.
+The program(s) may be used and/or copied only with
+the written permission of Greg R. Jacobs
+or in accordance with the terms and conditions
+stipulated in the agreement/contract under which
+the program(s) have been supplied.
+=============================================================*/
