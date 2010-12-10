76cf8c3deb190d9ade1793e0f3863bb69b0b1b82
greg.r.jacobs@gmail.com <greg.r.jacobs@gmail.com@6ecfc60f-d6d6-e1e8-1c63-cb8d0dac36c7>
2010-12-10 05:37:37 +0000

    Fixed TCP-SocketLibrary so that garrett could load

---
+++ trunk/Socket/TCP-SocketLibrary/SocketClient.cpp
@@ -0,0 +1,52 @@
+/** @file: SocketClient.cpp
+	@author Greg R. Jacobs
+	@author greg.r.jacobs@gmail.com
+	@author http://gregrjacobs.com
+	@date 2010-12-09
+	@version 0.0.0
+	@note Developed for Visual C++ 10.0
+	@brief ...
+	*/
+
+
+#include "SocketClient.hpp"
+
+SocketClient::SocketClient(const std::string& host, int port) : Socket() {
+	std::string error;
+
+	hostent *he;
+	if ((he = gethostbyname(host.c_str())) == 0) {
+	error = strerror(errno);
+	throw error;
+	}
+
+	sockaddr_in addr;
+	addr.sin_family = AF_INET;
+	addr.sin_port = htons(port);
+	addr.sin_addr = *((in_addr *)he->h_addr);
+	memset(&(addr.sin_zero), 0, 8); 
+
+	if (::connect(s_, (sockaddr *) &addr, sizeof(sockaddr))) {
+	error = strerror(WSAGetLastError());
+	throw error;
+	}
+}
+/*=============================================================
+
+Revision History
+
+Version 0.0.0: 2010-12-09
+Alpha release.
+
+=============================================================
+
+Copyright Greg R. Jacobs
+
+The copyright to the computer program(s) herein
+is the property of Greg R. Jacobs of Canada.
+The program(s) may be used and/or copied only with
+the written permission of Greg R. Jacobs
+or in accordance with the terms and conditions
+stipulated in the agreement/contract under which
+the program(s) have been supplied.
+=============================================================*/
